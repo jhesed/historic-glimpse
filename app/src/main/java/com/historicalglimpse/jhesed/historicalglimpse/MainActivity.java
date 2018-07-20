4e65ed55017ec9327872b229e895df659c357372
jhesed.tacadena <jhesed.tacadena@gmail.com>
2018-07-21 02:52:45 +0800

    v1.0: Integrated navigation bar with today glimpse

--- app/src/main/java/com/historicalglimpse/jhesed/historicalglimpse/MainActivity.java
+++ app/src/main/java/com/historicalglimpse/jhesed/historicalglimpse/MainActivity.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_today:
-                    mTextMessage.setText(R.string.title_today);
                     return true;
                 case R.id.navigation_month:
-                    mTextMessage.setText(R.string.title_month);
                     return true;
                 case R.id.navigation_about:
-                    mTextMessage.setText(R.string.title_about);
                     return true;
             }
             return false;
@@ -37,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

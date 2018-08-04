d98351373892e8aa97ac7c84affdcafc852a99f0
jhesed.tacadena <jhesed.tacadena@gmail.com>
2018-08-04 11:31:21 +0800

    fragment:  Added caching to retrofit

--- app/src/main/java/com/historicalglimpse/jhesed/historicalglimpse/APIClient.java
+++ app/src/main/java/com/historicalglimpse/jhesed/historicalglimpse/APIClient.java
@@ -1,10 +1,19 @@
 package com.historicalglimpse.jhesed.historicalglimpse;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,27 +27,78 @@ class APIClient {
 
     static Retrofit getClient(Context context) {
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
-        // Caching
-        int cacheSize = 1 * 1024 * 1024; // 1 MB
-        Cache cache = new Cache(context.getCacheDir(), cacheSize);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .cache(cache)
-                .build();
-
         retrofit = new Retrofit.Builder()
-                // TODO: Replace with corresponding url
-//                .baseUrl("http://192.168.43.91")
                 .baseUrl("http://jsoscc.com")
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
+                .client(provideOkHttpClient(context))
                 .build();
 
         return retrofit;
     }
 
+
+    private static OkHttpClient provideOkHttpClient(Context context) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(provideOfflineCacheInterceptor(context))
+                .addNetworkInterceptor(provideCacheInterceptor())
+                .cache(provideCache(context))
+                .build();
+    }
+
+    private static Cache provideCache(Context context) {
+        Cache cache = null;
+        try {
+            cache = new Cache(new File(context.getCacheDir(), "http-cache"),
+                    10 * 1024 * 1024); // 10 MB
+        } catch (Exception e) {
+//            Timer.e( e, "Could not create Cache!" );
+        }
+        return cache;
+    }
+
+    public static Interceptor provideCacheInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Response response = chain.proceed(chain.request());
+
+                // re-write response header to force use of cache
+                CacheControl cacheControl = new CacheControl.Builder()
+                        .maxAge(3, TimeUnit.MINUTES)
+                        .build();
+
+                return response.newBuilder()
+                        .header("Cache-Control", cacheControl.toString())
+                        .build();
+            }
+        };
+    }
+
+    public static Interceptor provideOfflineCacheInterceptor(final Context context) {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+
+                if (!isInternetAvailable(context)) {
+                    CacheControl cacheControl = new CacheControl.Builder()
+                            .maxStale(7, TimeUnit.DAYS)
+                            .build();
+
+                    request = request.newBuilder()
+                            .cacheControl(cacheControl)
+                            .build();
+                }
+
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    private static boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
